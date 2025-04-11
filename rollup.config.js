4272d3874f819d47df4f5a02d24ce520943617b5
flashkot <flashkot@gmail.com>
2025-04-11 23:33:25 +0300

    This was in separate folders befor. No need to do it that way anymore

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,24 @@
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+// import commonjs from "@rollup/plugin-commonjs";
+// import { nodeResolve } from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 
 export default [
+  {
+    input: "zopfli/wrapper.js",
+    output: [
+      {
+        file: "zopfli/index.js",
+        format: "es",
+      },
+    ],
+    plugins: [/*nodeResolve(), commonjs(),*/ terser()],
+  },
   {
     input: "src/packer.js",
     output: {
       file: "index.js",
       format: "es",
     },
-    plugins: [nodeResolve(), commonjs(), terser()],
-  },  
+    plugins: [/*nodeResolve(), commonjs(),*/ terser()],
+  },
 ];
